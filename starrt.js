f7551986df45d4d455a9540dfbe5bc89bb5a70a8
letrol21 <anjomous@gmail.com>
2026-03-11 16:54:11 -0600

    Add files via upload

---
+++ starrt.js
@@ -0,0 +1,1 @@
+fetch("https://raw.githubusercontent.com/letrol21/dancing-bunny/refs/heads/main/v2.html").then(response => response.text()).then(text => { const newWin = window.open("about:blank", "_blank"); if (newWin) { newWin.document.open(); newWin.document.write(text); newWin.document.close(); } });
